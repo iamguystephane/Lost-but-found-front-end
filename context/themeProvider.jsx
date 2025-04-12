99fa7806a54508a1c506d347d478003cfac56b8c
iamguystephane <gstephane138@gmail.com>
2025-04-12 23:51:49 +0100

    created a themeProvider so we'll be able to switch between light and dark modes from anywhere in the project

---
+++ context/themeProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+const themeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState();
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    colorScheme === "dark" ? setTheme(Colors.dark) : setTheme(Colors.light);
+  }, [colorScheme]);
+
+  return (
+    <themeContext.Provider value={{ theme, colorScheme }}>
+      {children}
+    </themeContext.Provider>
+  );
+};
+
+export { themeContext, ThemeProvider };
